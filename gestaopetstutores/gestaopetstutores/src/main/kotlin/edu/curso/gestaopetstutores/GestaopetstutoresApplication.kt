d5c0fffb88e43824da540fd293481198e76cd50e
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2025-03-29 00:28:00 -0300

    criação do projeto

---
+++ gestaopetstutores/gestaopetstutores/src/main/kotlin/edu/curso/gestaopetstutores/GestaopetstutoresApplication.kt
@@ -0,0 +1,11 @@
+package edu.curso.gestaopetstutores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestaopetstutoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestaopetstutoresApplication>(*args)
+}
---
+++ gestaopetstutores/gestaopetstutores/src/test/kotlin/edu/curso/gestaopetstutores/GestaopetstutoresApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.curso.gestaopetstutores
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GestaopetstutoresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
